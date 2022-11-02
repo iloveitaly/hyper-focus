c9e56510afe6a66d29ff19cd15335f3b7298e056
Michael Bianco <iloveitaly@gmail.com>
2022-11-02 13:41:42 -0600

    Bump min macos version, this fixed the weird linking issue I was running into
    ❯ swift run
    Building for debugging...
    Build complete! (0.25s)
    dyld[21481]: Symbol not found: (_$s10Foundation11JSONDecoderC6decode_4fromxxm_AA4DataVtKSeRzlFTj)
      Referenced from: '/Users/mike/Projects/focus-app/.build/x86_64-apple-macosx/debug/focus-app'
      Expected in: '/System/Library/Frameworks/Foundation.framework/Versions/C/Foundation'
    [1]    21481 abort      swift run

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "focus-app",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v10_14)
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
