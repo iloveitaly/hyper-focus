827c69196197729e9e09e53b9e0dc43abc1a626c
Michael Bianco <iloveitaly@gmail.com>
2022-11-03 14:39:59 -0600

    Switch to ambassador

--- Package.swift
+++ Package.swift
@@ -9,15 +9,14 @@ let package = Package(
         .macOS(.v10_15)
     ],
     dependencies: [
-        .package(url: "https://github.com/Building42/Telegraph.git", from: "0.30.0"),
-        .package(url: "https://github.com/envoy/Embassy.git", from: "4.1.4"),
+        .package(url: "https://github.com/envoy/Ambassador.git", from: "4.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "focus-app",
-            dependencies: ["Telegraph", "Embassy"]),
+            dependencies: ["Ambassador"]),
         .testTarget(
             name: "focus-appTests",
             dependencies: ["focus-app"]),
