faa2a5f9a2bb16d384f7cebe311d512aea8bf91f
Michael Bianco <iloveitaly@gmail.com>
2022-11-08 12:33:13 -0700

    Adding status endpoint

--- Sources/focus-app/server.swift
+++ Sources/focus-app/server.swift
@@ -1,16 +1,21 @@
-import Embassy
 import Ambassador
+import Embassy
 import Foundation
 import Telegraph
 
 var server: Server?
 
+// because I don't want to learn how to receive applescript events, and because I know
+// web technologies well, I decided to expose a http interface to the application instead
+// applescript, swiftui, etc.
+
 class ApiServer {
   let scheduleManager: ScheduleManager
+  let serverPort = 9029
 
-  // TODO not used!
+  // TODO: not used! Telegraph is a better server, but I did not realize how to get it working until later
   // http -vvv GET http://localhost:8081/ping
-  func old() {
+  func telegraphServer() {
     server = Server()
     guard let unwrappedServer = server else {
       return
@@ -30,32 +35,63 @@ class ApiServer {
 
   init(scheduleManager: ScheduleManager) {
     self.scheduleManager = scheduleManager
-    old()
 
     // must run in separate thread or it will block the main event loop
     Task {
       let loop = try! SelectorEventLoop(selector: try! KqueueSelector())
       let router = Router()
-      let server = DefaultHTTPServer(eventLoop: loop, port: 8080, app: router.app)
+      let server = DefaultHTTPServer(eventLoop: loop, port: serverPort, app: router.app)
 
       // https://github.com/envoy/Ambassador/blob/master/Ambassador/Responses/JSONResponse.swift
 
-      router["/ping"] = JSONResponse() { _ -> Any in
-        return ["status": "ok"]
+      router["/ping"] = JSONResponse { _ -> Any in
+        ["status": "ok"]
       }
 
-      router["/configurations"] = JSONResponse() { _ -> Any in
-        return self.scheduleManager.namedSchedules().map { $0.name }
+      router["/status"] = JSONResponse { _ -> [String: Dictionary] in
+        let currentSchedule = self.scheduleManager.schedule
+        var scheduledUntil: Int?
+
+        if currentSchedule != nil {
+          let nowWithStartAndEnd = Calendar.current.date(
+            bySettingHour: currentSchedule!.start!,
+            minute: currentSchedule!.end!,
+            second: 0,
+            of: Date()
+          )!
+
+          scheduledUntil = Int(nowWithStartAndEnd.timeIntervalSince1970)
+        }
+
+        return [
+          "override": [
+            "until": self.scheduleManager.endOverride?.timeIntervalSince1970,
+            "name": self.scheduleManager.overrideSchedule?.name,
+          ],
+          "pause": [
+            "until": self.scheduleManager.endPause?.timeIntervalSince1970,
+          ],
+          "schedule": [
+            "name": self.scheduleManager.schedule?.name,
+            "until": scheduledUntil
+          ],
+        ]
+      }
+
+      router["/configurations"] = JSONResponse { _ -> Any in
+        self.scheduleManager.namedSchedules().map { $0.name }
       }
 
       // http -vvv POST http://localhost:8080/override name='hyper focus' until=1667514400
-      router["/override"] = JSONResponse() { environ -> Any in
+      router["/override"] = JSONResponse { environ -> Any in
+        log("/override")
+
         var pauseUtil: Int?
         var scheduleName: String?
 
         let input = environ["swsgi.input"] as! SWSGIInput
         JSONReader.read(input) { json in
-          guard let receivedData: [String : Any] = json as? [String: Any] else {
+          guard let receivedData: [String: Any] = json as? [String: Any] else {
             return
           }
 
@@ -83,12 +119,14 @@ class ApiServer {
       }
 
       // http -vvv POST http://localhost:8080/pause until=1667510271
-      router["/pause"] = JSONResponse() { environ -> Any in
+      router["/pause"] = JSONResponse { environ -> Any in
+        log("pause route")
+
         var pauseUtil: Int?
 
         let input = environ["swsgi.input"] as! SWSGIInput
         JSONReader.read(input) { json in
-          guard let receivedData: [String : Any] = json as? [String: Any] else {
+          guard let receivedData: [String: Any] = json as? [String: Any] else {
             return
           }
 
