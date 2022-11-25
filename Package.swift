204ab16da1025d505ef59746832599896fb7b711
Michael Bianco <iloveitaly@gmail.com>
2022-11-25 12:20:55 -0700

    Rename app

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "focus-app",
+    name: "hyper-focus",
     platforms: [
         .macOS(.v10_15)
     ],
@@ -15,10 +15,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
-            name: "focus-app",
+            name: "hyper-focus",
             dependencies: ["Ambassador"]),
         .testTarget(
-            name: "focus-appTests",
-            dependencies: ["focus-app"]),
+            name: "hyper-focusTests",
+            dependencies: ["hyper-focus"]),
     ]
 )
