f840930a64fa5d66a01bb677f52a7669829f5095
Michael Bianco <iloveitaly@gmail.com>
2022-11-26 10:23:31 -0700

    Fixing tests

--- Tests/hyper-focusTests/focus_appTests.swift
+++ Tests/hyper-focusTests/focus_appTests.swift
@@ -1,11 +1,11 @@
 import XCTest
 @testable import hyper_focus
 
-final class focus_appTests: XCTestCase {
+final class hyper_focusTests: XCTestCase {
     // this should actually be false, but the cocoa library seems to fail on `[]` in query strings
     func testURLEquality() throws {
-        XCTAssertEqual(false, ActionHandler.isSubsetOfUrl(
-            supersetUrlString: "http://monica.hole/people?tags[]=vc&othr=var",
+        XCTAssertEqual(true, ActionHandler.isSubsetOfUrl(
+            supersetUrlString: "http://monica.hole/people?tags[]=vc&other=var",
             subsetUrlString: "http://monica.hole/people?tags[]=vc"
         ))
     }
