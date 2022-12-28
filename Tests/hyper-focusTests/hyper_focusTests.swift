8b6176a6ce27f3ea751f69397c39807fbcc088d9
Michael Bianco <iloveitaly@gmail.com>
2022-12-28 07:18:42 -0700

    addingf another url test

--- Tests/hyper-focusTests/hyper_focusTests.swift
+++ Tests/hyper-focusTests/hyper_focusTests.swift
@@ -2,8 +2,18 @@ import XCTest
 @testable import hyper_focus
 
 final class hyper_focusTests: XCTestCase {
-    // this should actually be false, but the cocoa library seems to fail on `[]` in query strings
     func testURLEquality() throws {
+        XCTAssertEqual(true,
+            ActionHandler.isSubsetOfUrl(
+                supersetUrlString: "https://www.google.com/search?client=safari&rls=en&q=world+news&ie=UTF-8&oe=UTF-8",
+                subsetUrlString: "https://www.google.com/search?q=world+news"
+            )
+        )
+    }
+
+    func testArrayURLEquality() throws {
+        // the cocoa library for parsing query strings seems to fail on `[]` in query strings
+        // on specific macos versions
         XCTAssertEqual(true, ActionHandler.isSubsetOfUrl(
             supersetUrlString: "http://monica.hole/people?tags[]=vc&other=var",
             subsetUrlString: "http://monica.hole/people?tags[]=vc"
