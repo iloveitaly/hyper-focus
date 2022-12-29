de99be91e4c1b8208093f01e4fc5f088f13e70f3
Michael Bianco <iloveitaly@gmail.com>
2022-12-29 09:21:29 -0700

    adding swift argument parser

--- Package.swift
+++ Package.swift
@@ -9,14 +9,18 @@ let package = Package(
         .macOS(.v10_15)
     ],
     dependencies: [
-        .package(url: "https://github.com/envoy/Ambassador.git", from: "4.0.0"),
+        .package(url: "https://github.com/envoy/Ambassador", from: "4.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "hyper-focus",
-            dependencies: ["Ambassador"]),
+            dependencies: [
+                "Ambassador",
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]),
         .testTarget(
             name: "hyper-focusTests",
             dependencies: ["hyper-focus"]),
