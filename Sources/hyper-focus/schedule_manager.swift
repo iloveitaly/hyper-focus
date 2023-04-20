18c9d726c683e83829fe82f59625dfe2eb6c6957
Michael Bianco <iloveitaly@gmail.com>
2023-04-20 10:52:28 -0600

    refactor: extract out configuration loader

--- Sources/hyper-focus/schedule_manager.swift
+++ Sources/hyper-focus/schedule_manager.swift
@@ -1,32 +1,5 @@
 import Foundation
 
-enum ConfigurationLoader {
-    // easier to set this globally than to pass it around everywhere
-    static var userConfigurationPath: String?
-
-    static func loadConfiguration() -> Configuration {
-        return loadConfigurationFromPath(userConfigurationPath)
-    }
-
-    private static func loadConfigurationFromPath(_ userConfigPath: String?) -> Configuration {
-        var configPath: URL?
-
-        // is userConfigPath a valid file URL?
-        if userConfigPath != nil && FileManager.default.fileExists(atPath: userConfigPath!) {
-            configPath = URL(fileURLWithPath: userConfigPath!)
-        } else {
-            configPath = URL(fileURLWithPath: NSHomeDirectory()).appendingPathComponent(".config/focus/config.json")
-        }
-
-        debug("Loading configuration from \(configPath!.absoluteString)")
-
-        let configData = try! Data(contentsOf: configPath!)
-        let config = try! JSONDecoder().decode(Configuration.self, from: configData)
-
-        return config
-    }
-}
-
 class ScheduleManager {
     var configuration: Configuration
 
