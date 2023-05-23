5600878474f1fd71a835b0c6725137361d7b6b78
Michael Bianco <iloveitaly@gmail.com>
2023-05-23 07:22:04 -0600

    docs: todo update

--- Tests/hyper-focusTests/hyper_focusTests.swift
+++ Tests/hyper-focusTests/hyper_focusTests.swift
@@ -30,4 +30,52 @@ final class hyper_focusTests: XCTestCase {
         XCTAssertEqual(true,
                        ActionHandler.isSubsetOfUrl(supersetUrlString: exampleUrl, subsetUrlString: exampleUrl))
     }
+
+    func testInitialWakeNotification() throws {
+        let previousDayTimestamp: TimeInterval = 1_677_355_905
+        let nextDayTimestamp: TimeInterval = 1_677_441_607
+
+        let configuration = mockConfiguration()
+        let scheduleManager = ScheduleManager(configuration)
+        let sleepWatcher = SleepWatcher(scheduleManager: scheduleManager, dateGenerator: { Date(timeIntervalSince1970: nextDayTimestamp) })
+
+        // TODO: we should assert that the wake script is actually run in this case, but this will require some refactoring
+        sleepWatcher.lastWakeTime = Date(timeIntervalSince1970: previousDayTimestamp)
+        sleepWatcher.awakeFromSleep(mockWakeNotification())
+    }
+
+    // TODO: mmm these dates don't look right
+    func testInitialWakeNotificationWhichShouldNotFire() throws {
+        // 2023-04-18 18:03:37 +0000
+        // April 18, 2023 at 12:03:37 PM Mountain Daylight Time
+        let previousDayInUTCTimestamp = 1_681_841_017
+        // 2023-04-19 23:39:08 +0000
+        // April 19, 2023 at 5:39:08 PM Mountain Daylight Time
+        let currentDayTimestamp = 1_681_947_548
+    }
+
+    func mockWakeNotification() -> Notification {
+        let mockNotification = Notification(name: NSWorkspace.didWakeNotification, object: nil, userInfo: nil)
+        return mockNotification
+    }
+
+    func mockConfiguration() -> Configuration {
+        let testScheduleItem = Configuration.ScheduleItem(
+            start: 8 * 60,
+            end: 18 * 60,
+            name: "Work Hours",
+            start_script: "/path/to/start_script.sh",
+            block_hosts: ["example.com"],
+            block_urls: ["example.org"],
+            block_apps: ["AppName"]
+        )
+
+        let testConfiguration = Configuration(
+            initial_wake: "/path/to/initial_wake.sh",
+            wake: "/path/to/wake.sh",
+            schedule: [testScheduleItem]
+        )
+
+        return testConfiguration
+    }
 }
