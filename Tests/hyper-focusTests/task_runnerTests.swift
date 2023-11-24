c8ecf348fef2acd41654e6867cded573b6dc78a8
Michael Bianco <iloveitaly@gmail.com>
2023-11-24 10:37:21 -0700

    test: adding parallel execution tests

--- Tests/hyper-focusTests/task_runnerTests.swift
+++ Tests/hyper-focusTests/task_runnerTests.swift
@@ -11,9 +11,36 @@ final class task_runnerTests: XCTestCase {
         // try running again
         TaskRunner.executeTaskWithName(tempFile.path, "script_start")
 
-        // defer is like finally in other languages
-        defer {
-            try! FileManager.default.removeItem(at: tempFile)
+        do {
+            try FileManager.default.removeItem(at: tempFile)
+        } catch {
+            XCTFail("Error cleaning up temporary file: \(error)")
+        }
+    }
+
+    func testParallelNonExecutableScriptRun() throws {
+        let tempFile = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("test.sh")
+        try! "sleep 30; echo 'execution error: Flow got an error: AppleEvent timed out.' >&2; exit 1".write(to: tempFile, atomically: true, encoding: .utf8)
+
+        let expectation1 = expectation(description: "First script completion")
+        let expectation2 = expectation(description: "Second script completion")
+
+        DispatchQueue.global().async {
+            TaskRunner.executeTaskWithName(tempFile.path, "script_start")
+            expectation1.fulfill()
+        }
+
+        DispatchQueue.global().async {
+            TaskRunner.executeTaskWithName(tempFile.path, "script_start")
+            expectation2.fulfill()
+        }
+
+        wait(for: [expectation1, expectation2], timeout: 35.0)
+
+        do {
+            try FileManager.default.removeItem(at: tempFile)
+        } catch {
+            XCTFail("Error cleaning up temporary file: \(error)")
         }
     }
 }
